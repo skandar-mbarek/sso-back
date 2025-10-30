34ae4813204d600d52c3279cb494b9058b6541ba
Skandar M. <skandar.m@trvlcode.com>
2025-10-30 12:35:12 +0100

    fix dockerfile

--- sso-back/obj/Debug/net9.0/sso-back.AssemblyInfo.cs
+++ sso-back/obj/Debug/net9.0/sso-back.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sso-back")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de68f3a951014372a0af3103ac2aa12f28fe8317")]
 [assembly: System.Reflection.AssemblyProductAttribute("sso-back")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sso-back")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
