ecdee171726da3a84782c13e53b7a3ed11823124
Skandar M. <skandar.m@trvlcode.com>
2025-11-04 10:37:36 +0100

    update cookies to Strict

--- sso-back/obj/Debug/net9.0/sso-back.AssemblyInfo.cs
+++ sso-back/obj/Debug/net9.0/sso-back.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sso-back")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edb3841bed35a8c19a9b9d970b9b896a9c24dc6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+133a9d68be982d071abdf81f615a5ece1692a688")]
 [assembly: System.Reflection.AssemblyProductAttribute("sso-back")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sso-back")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
